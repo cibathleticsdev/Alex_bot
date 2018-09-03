7262fa140fbf57e42cd7594b8c47fd475aab93e5
PyJavaPulver <40663649+PyJavaPulver@users.noreply.github.com>
2018-09-03 13:26:22 +0200

    Update alex_bot.py

--- alex_bot.py
+++ alex_bot.py
@@ -112,8 +112,8 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    await client.send_message(discord.Object(id='481951758722138113'), 'YourBoss is now online!')
-    await client.change_presence(game=discord.Game(name="Fortnite"))
+    #await client.send_message(discord.Object(id='481951758722138113'), 'YourBoss is now online!')
+    #await client.change_presence(game=discord.Game(name="Fortnite"))
 
 
 if __name__ == '__main__':
